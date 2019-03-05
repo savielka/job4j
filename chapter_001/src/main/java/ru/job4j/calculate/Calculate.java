2fea2f13e9ffc4ebe5f9a5fdd10be25915cec0e9
savielka <tata.savelieva97@gmail.com>
2019-03-05 21:09:44 +0300

    1.3 JavaDoc. Code convention [#117620]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -6,6 +6,10 @@ package ru.job4j.calculate;
  */
 
 public class Calculate {
+	/**
+	 * Main.
+	 * @param args - args.
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
