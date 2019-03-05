d39096d1a6fd5eeb468490a008439e9e3df47070
savielka <tata.savelieva97@gmail.com>
2019-03-05 20:15:58 +0300

    JavaDoc. Code convention [#117620]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,10 @@
 package ru.job4j.calculate;
 
+/**
+ * Class Класс для вычисления арифметических операций
+ * @author savielka
+ */
+
 public class Calculate {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Package for calculate task.
+ *
+ * @author savielka
+ * @version $1.0$
+ * @since 05.03.2019
+ */
+ 
+package ru.job4j.calculate;
