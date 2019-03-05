aa3318509d53bee6d03a6165c24fe54b50ed5472
savielka <tata.savelieva97@gmail.com>
2019-03-05 21:35:42 +0300

    1.4 JUnit[#117621]

---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*
+* @author savielka
+* @version $1.0$
+* @since 05.03.2019
+*/
+public class CalculateTest {
+/**
+* Test echo.
+*/
+@Test
+public void whenTakeNameThenThreeEchoPlusName() {
+    String input = "Petr Arsentev";
+    String expect = "Echo, echo, echo : Petr Arsentev"; 
+    Calculate calc = new Calculate();
+    String result = calc.echo(input);
+    assertThat(result, is(expect));
+}
+ 
+}
---
+++ chapter_001/src/test/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author savielka
+* @version $1.0$
+* @since 05.03.2019
+*/
+package ru.job4j.calculate;
