50cd20b76ae8a96f5a59a40c4e261563545fbbd8
savielka <tata.savelieva97@gmail.com>
2019-03-06 13:33:01 +0300

    4.2. Максимум из двух чисел[#117584]

---
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+/**
+ * Class Max Поиск максимального из двух чисел.
+ * @author savielka
+ * @since 06.03.2019
+ * @version 0.1
+ */
+public class Max {
+    public int max(int first, int second) {
+        return ((first > second) ? first : second);
+    }
+}
