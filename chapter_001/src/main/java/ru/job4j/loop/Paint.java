5bfc5d0f2bd49aaef7f48f05af3b79caf1f31e18
savielka <tata.savelieva97@gmail.com>
2019-03-13 20:24:47 +0300

    5.4. Построить пирамиду в псевдографике.[#117594]

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,11 +1,51 @@
 package ru.job4j.loop;
 
 public class Paint {
+    public String rightTrl(int height) {
+        // Буфер для результата.
+        StringBuilder screen = new StringBuilder();
+        // ширина будет равна высоте.
+        int widht = height;
+        // внешний цикл двигается по строкам.
+        for (int row = 0; row != height; row++) {
+            // внутренний цикл определяет положение ячейки в строке.
+            for (int column = 0; column != widht; column++) {
+                // если строка равна ячейки, то рисуем галку.
+                // в данном случае строка определяем, сколько галок будет на строке
+                if (row >= column) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            // добавляем перевод строки.
+            screen.append(System.lineSeparator());
+        }
+        // Получаем результат.
+        return screen.toString();
+    }
+
+    public String leftTrl(int height) {
+        StringBuilder screen = new StringBuilder();
+        int widht = height;
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != widht; column++) {
+                if (row >= widht - column - 1) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(System.lineSeparator());
+        }
+        return screen.toString();
+    }
+
     public String pyramid(int height) {
         StringBuilder screen = new StringBuilder();
-        int width = 2 * height - 1;
+        int widht = 2 * height - 1;
         for (int row = 0; row != height; row++) {
-            for (int column = 0; column != width; column++) {
+            for (int column = 0; column != widht; column++) {
                 if (row >= height - column - 1 && row + height - 1 >= column) {
                     screen.append("^");
                 } else {
--- chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -7,7 +7,41 @@ import static org.junit.Assert.*;
 
 public class PaintTest {
     @Test
+    public void whenPyramid4Right() {
+        Paint paint = new Paint();
+        String rst = paint.rightTrl(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("^   ")
+                                .add("^^  ")
+                                .add("^^^ ")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+
+    @Test
     public void whenPyramid4Left() {
+        Paint paint = new Paint();
+        String rst = paint.leftTrl(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   ^")
+                                .add("  ^^")
+                                .add(" ^^^")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+
+    @Test
+    public void whenPyramid4() {
         Paint paint = new Paint();
         String rst = paint.pyramid(4);
         System.out.println(rst);
@@ -24,7 +58,7 @@ public class PaintTest {
     }
 
     @Test
-    public void whenPyramid3Left() {
+    public void whenPyramid3() {
         Paint paint = new Paint();
         String rst = paint.pyramid(3);
         System.out.println(rst);
