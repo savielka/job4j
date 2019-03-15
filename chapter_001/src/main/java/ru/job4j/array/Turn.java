366baa6bcd405b0f280b293fc6f9b3e4307e8acd
savielka <tata.savelieva97@gmail.com>
2019-03-15 20:33:15 +0300

    6.5. Создать программу для сортировки массива методом перестановки.[#117603]

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -3,7 +3,7 @@ package ru.job4j.array;
 public class Turn {
     public int[] back(int[] array) {
         int tmp;
-        for (int i = 0; i < (array.length/2); i++) {
+        for (int i = 0; i < (array.length / 2); i++) {
             tmp = array[i];
             array[i] = array[array.length - i - 1];
             array[array.length - i - 1] = tmp;
